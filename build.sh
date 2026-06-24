26b48cc742332f0c8a6ac9a2f29653f6ed9b7aa7
faslarah <faseelarahma987@gmail.com>
2026-06-24 14:28:55 +0530

    Auto-create superuser on build

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Automatically create a superuser if it doesn't exist
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin123').exists() or User.objects.create_superuser('admin123', 'admin@openmall.com', 'admin123')"
